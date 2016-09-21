0f73d9b37436f73a735c29a92aff9702f03b639e
Jack McAuliffe <jack.mcauliffe01@gmail.com>
2016-09-22 00:36:23 +0100

    composite view controller

--- ListowelRaces/Racecard/CompositeTableViewDataSource.swift
+++ ListowelRaces/Racecard/CompositeTableViewDataSource.swift
@@ -17,22 +17,18 @@ class CompositeTableViewDataSource: NSObject, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        NSLog("get cell for row \(indexPath.row) in section \(indexPath.section)")
         let dataSource = sectionDataSources[indexPath.section]
         let newIndex = NSIndexPath(forRow: indexPath.row, inSection: 0)
-        NSLog("translated to row \(newIndex.row) in section \(newIndex.section)")
         return dataSource.tableView(tableView, cellForRowAtIndexPath: newIndex)
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        NSLog("there are \(sectionDataSources.count) sections")
         return sectionDataSources.count
     }
     
     func tableView(tableView: UITableView,
         numberOfRowsInSection section: Int) -> Int {
         let rows = sectionDataSources[section].tableView(tableView, numberOfRowsInSection: 0)
-        NSLog("there are \(rows) in section \(section)")
         return rows
     }
 }
