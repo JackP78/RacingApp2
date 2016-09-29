fba38e6c18936e995161e2408fb986224fda9ebd
Jack McAuliffe <jack.mcauliffe01@gmail.com>
2016-09-29 21:57:19 +0100

    moving to firebase

---
+++ ListowelRaces/Core/BestDressedEntry.swift
@@ -0,0 +1,17 @@
+//
+//  BestDressedEntry.swift
+//  ListowelRaces
+//
+//  Created by Jack McAuliffe on 29/09/2016.
+//  Copyright © 2016 Jack McAuliffe. All rights reserved.
+//
+
+import Foundation
+
+class BestDressedEntry : NSObject {
+    var email : String?
+    var userId: String?
+    var name: String?
+    var url: String?
+    var votes: NSNumber?
+}
