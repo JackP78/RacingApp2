11d43b9e3b822e4cbed86b0aea7544e2112660ca
Jack McAuliffe <jmcauliffe@fb.com>
2017-02-09 00:50:41 +0000

    Adding columnar header

---
+++ ListowelRaces/PrototypeCells/ColumnarHeader.swift
@@ -0,0 +1,21 @@
+//
+//  ColumnarHeader.swift
+//  ListowelRaces
+//
+//  Created by Jack McAuliffe on 2/8/17.
+//  Copyright © 2017 Jack McAuliffe. All rights reserved.
+//
+
+import UIKit
+
+class ColumnarHeader: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
